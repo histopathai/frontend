744204a88e90a3132d9ef3247b34708dac0949b2
yasintuncer <yasintuncerr@outlook.com>
2025-11-07 18:00:36 +0300

    Refactor IImageRepository: update interfaces for image upload and creation for improved clarity

--- src/core/repositories/IImageRepository.ts
+++ src/core/repositories/IImageRepository.ts
@@ -1,9 +1,14 @@
 import {Image} from '../entities/Image';
 import { PaginatedResult, Pagination } from '../types/common';
 
+
+export interface ImageUploadPayload {
+  uploadUrl: string;
+  headers: Record<string, string>;
+}
+
 export interface CreateImageRequest {
     patientId: string;
-    creatorId: string;
     contentType: string;
     name: string;
     format: string;
@@ -12,16 +17,14 @@ export interface CreateImageRequest {
     size?: number;
 }
 
-export interface UploadImageRequest {
-    uploadUrl: string;
-    Header: Record<string, string>;
-    file?: File;
+export interface UploadImageParams {
+  payload: ImageUploadPayload; 
+  file: File;
 }
 
 export interface IImageRepository {
-    list(pagination: Pagination): Promise<PaginatedResult<Image>>;
-    create(request: CreateImageRequest): Promise<UploadImageRequest>; // Return signed URL and headers for upload
-    upload(payload: UploadImageRequest): Promise<void>; // Upload actual file to storage
+    create(request: CreateImageRequest): Promise<ImageUploadPayload>;
+    upload(params: UploadImageParams): Promise<void>;
     getById(imageId: string): Promise<Image>;
     delete(imageId: string): Promise<void>;
     getByPatientId(patientId: string, pagination: Pagination): Promise<PaginatedResult<Image>>;
