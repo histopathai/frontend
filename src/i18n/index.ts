7fb14a129d44476a1620ebb7ce156d5fa9f1f656
yasintuncer <yasintuncerr@outlook.com>
2025-11-17 00:24:35 +0300

    refactor: streamline i18n message loading by using dynamic imports for locale files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,40 @@
 import { createI18n } from 'vue-i18n';
-import trErrors from '@/locales/tr/errors.json';
-import enErrors from '@/locales/en/errors.json';
+
+const messages: {
+  en: Record<string, any>;
+  tr: Record<string, any>;
+} = {
+  en: {},
+  tr: {},
+};
+
+function extractFileName(path: string) {
+  return path.split('/').pop()!.replace('.json', '');
+}
+
+// EN files
+const enModules = import.meta.glob('@/locales/en/*.json', { eager: true }) as Record<
+  string,
+  { default: Record<string, any> }
+>;
+for (const path in enModules) {
+  const fileName = extractFileName(path);
+  messages.en[fileName] = enModules[path]?.default ?? {};
+}
+
+// TR files
+const trModules = import.meta.glob('@/locales/tr/*.json', { eager: true }) as Record<
+  string,
+  { default: Record<string, any> }
+>;
+for (const path in trModules) {
+  const fileName = extractFileName(path);
+  messages.tr[fileName] = trModules[path]?.default ?? {};
+}
 
 export const i18n = createI18n({
   legacy: false,
-  locale: 'tr', // default dil
-  messages: {
-    tr: { errors: trErrors },
-    en: { errors: enErrors },
-  },
+  locale: 'tr',
+  fallbackLocale: 'en',
+  messages,
 });
