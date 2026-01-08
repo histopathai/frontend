31837b5042688541ff765612c424f0320a027e94
cilemafacan <afacan.cilem@gmail.com>
2026-01-08 23:47:58 +0300

    feat: Update CreateNewAnnotationRequest to include additional fields for improved annotation data handling

--- src/core/repositories/IAnnotation.ts
+++ src/core/repositories/IAnnotation.ts
@@ -1,14 +1,20 @@
 import { Annotation } from '../entities/Annotation';
 import type { PaginatedResult, Pagination } from '../types/common';
 import { Point } from '../value-objects/Point';
+import type { TagValue } from '../types/tags';
 
 export interface CreateNewAnnotationRequest {
   image_id: string;
   annotator_id: string;
+
+  annotation_type_id: string;
+
   polygon: Point[];
+  data?: TagValue[];
+
+  description?: string;
   score?: number;
   class?: string;
-  description?: string;
 }
 
 export interface IAnnotationRepository {
